fabd6dcbe889ce5018d3229aafb3a4360bb349a7
ajh123 <samrosoft36@gmail.com>
2021-07-18 19:15:01 +0100

    docstrings for main and editor

--- editor.py
+++ editor.py
@@ -1,5 +1,9 @@
 from engine.windows import EditPygletWindow
 import pyglet
 
+"""Editor.py
+the game editor for the engine
+"""
+
 win = EditPygletWindow(60)  # set the fps
 pyglet.app.run()
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from engine.windows import PygletWindow
 import pyglet
 
+"""main.py
+the game it self for the engine
+"""
+
 win = PygletWindow(60)  # set the fps
 pyglet.app.run()
